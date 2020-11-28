2eb1aa2f8e0b4bf839cdc3941321b28b98235e37
patoarvizu <patoarvizu@gmail.com>
2020-11-28 14:49:51 -0500

    Set parameters as group parameters

--- vfp.py
+++ vfp.py
@@ -11,8 +11,7 @@ import time
 
 def __initSensor():
     try:
-        f = PyFingerprint('/dev/ttyUSB0', 57600, 0xFFFFFFFF, 0x00000000)
-
+        f = PyFingerprint('/dev/tty.usbserial-AR0JX6N2', 57600, 0xFFFFFFFF, 0x00000000)
         if ( f.verifyPassword() == False ):
             raise ValueError('The given fingerprint sensor password is wrong!')
 
@@ -45,15 +44,25 @@ def __readUntilFound(f):
             continue
 
 @click.group()
-def main():
+@click.option('-address', default='http://127.0.0.1:8200')
+@click.option('-encryption-key-file', default="fingerprint-encryption.key")
+@click.option('-encryption-init-output-file', default="encrypted-init-output.json")
+@click.pass_context
+def main(ctx, address):
+    ctx.obj = {
+        'address': address,
+        'encryption_key_file': encryption_key_file,
+        'encryption_init_output_file': encryption_init_output_file
+    }
     pass
 
 @main.command()
-@click.option('-address', default='http://127.0.0.1:8200')
 @click.option('-key-shares', default=5, type=int)
-@click.option('-encryption-key-file', default="fingerprint-encryption.key")
-@click.option('-encryption-init-output-file', default="encrypted-init-output.json")
-def init(address, key_shares, encryption_key_file, encryption_init_output_file):
+@click.pass_context
+def init(ctx, key_shares):
+    address = ctx.obj['address']
+    encryption_key_file = ctx.obj['encryption_key_file']
+    encryption_init_output_file = ctx.obj['encryption_init_output_file']
     f = __initSensor()
     __readUntilFound(f)
 
@@ -64,18 +73,15 @@ def init(address, key_shares, encryption_key_file, encryption_init_output_file):
         payload = { 'secret_shares': key_shares, 'secret_threshold': key_shares, 'stored_shares': key_shares, 'recovery_shares': key_shares, 'recovery_threshold': key_shares }
         r = requests.put(address + '/v1/sys/init', data=json.dumps(payload), headers = { 'X-Vault-Request': 'true' })
         u = json.loads(r.text)
-
         encrypted_init_output = {}
         encrypted_keys = []
         for k in u["keys"]:
             encrypted_keys.append(f.encrypt(k.encode()).decode())
 
         encrypted_init_output['encrypted_keys'] = encrypted_keys
-
         o = open(encryption_init_output_file, "w")
         o.write(json.dumps(encrypted_init_output))
         o.close()
-
         key_file = open(encryption_key_file, "w")
         key_file.write(key.decode())
         key_file.close()
@@ -90,10 +96,11 @@ def init(address, key_shares, encryption_key_file, encryption_init_output_file):
         exit(1)
 
 @main.command()
-@click.option('-address', default='http://127.0.0.1:8200')
-@click.option('-encryption-key-file', default="fingerprint-encryption.key")
-@click.option('-encryption-init-output-file', default="encrypted-init-output.json")
-def unseal(address, encryption_key_file, encryption_init_output_file):
+@click.pass_context
+def unseal(ctx):
+    address = ctx.obj['address']
+    encryption_key_file = ctx.obj['encryption_key_file']
+    encryption_init_output_file = ctx.obj['encryption_init_output_file']
     f = __initSensor()
     __readUntilFound(f)
 
@@ -114,26 +121,23 @@ def unseal(address, encryption_key_file, encryption_init_output_file):
         exit(1)
 
 @main.command()
-@click.option('-address', default='http://127.0.0.1:8200')
-@click.option('-encryption-key-file', default="fingerprint-encryption.key")
-@click.option('-encryption-init-output-file', default="encrypted-init-output.json")
-def generate_root(address, encryption_key_file, encryption_init_output_file):
+@click.pass_context
+def generate_root(ctx):
+    address = ctx.obj['address']
+    encryption_key_file = ctx.obj['encryption_key_file']
+    encryption_init_output_file = ctx.obj['encryption_init_output_file']
     f = __initSensor()
     __readUntilFound(f)
-
     try:
         key_file = open(encryption_key_file, "r")
         f = Fernet(key_file.read())
-
         o = open(encryption_init_output_file, "r")
         e = json.loads(o.read())
-
         r = requests.put(address + '/v1/sys/generate-root/attempt')
         attempt = json.loads(r.text)
         nonce = attempt["nonce"]
         otp = attempt["otp"]
         progress = None
-
         for unseal_key in e["encrypted_keys"]:
             payload = { 'key': f.decrypt(unseal_key.encode()).decode(), 'nonce': nonce }
             r = requests.put(address + '/v1/sys/generate-root/update', data=json.dumps(payload))
@@ -154,7 +158,6 @@ def generate_root(address, encryption_key_file, encryption_init_output_file):
 @main.command()
 def enroll():
     f = __initSensor()
-
     while True:
         try:
             print('Waiting for finger...')
